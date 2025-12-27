fa177e87e0aee175b423b133a0e2c3ca35ddbda8
azizullosultanov4-ops <azizullosultanov4@gmail.com>
2025-12-27 19:04:03 +0300

    kk

--- WebMaster/urls.py
+++ WebMaster/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from apps.main.views import index 
+from apps.core.views import index 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name = "index_page" ), 
